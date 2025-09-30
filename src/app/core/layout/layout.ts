457726d24256a47744b11f0139714241b0a1459c
WolandEmort <lilipunhaaa@gmail.com>
2025-09-30 16:22:08 +0300

    Оновив структуру та хедер

---
+++ src/app/core/layout/layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  imports: [Headers],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css'
+})
+export class Layout {
+
+}
